d0d874ec77f4a871a96a3c1d5dc13f7b2648ee6b
1226151667 <1226151667@qq.com>
2018-02-02 13:29:26 +0800

    we

--- admin/index/controller/Nolapi.php
+++ admin/index/controller/Nolapi.php
@@ -6,6 +6,7 @@ use app\index\model\Website;
 class Nolapi extends Controller{
 	public function login_status(){
 		$callback = request()->get('callback');
+		header('Content-Type:text/javascript;charset=utf-8');
 		if(!session('?empuser_uname') || !session('?empuser_id')){
 			// return $callback.'(100)';exit;
 			return 'alert(100)';
