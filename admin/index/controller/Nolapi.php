d7f3be32f9cf4aa65709b1ada665b76c727b2db4
1226151667 <1226151667@qq.com>
2018-02-02 13:25:37 +0800

    we

--- admin/index/controller/Nolapi.php
+++ admin/index/controller/Nolapi.php
@@ -7,22 +7,8 @@ class Nolapi extends Controller{
 	public function login_status(){
 		$callback = request()->get('callback');
 		if(!session('?empuser_uname') || !session('?empuser_id')){
-			return $callback.'("no")';exit;
-			// return json_encode(['status'=>'no', 'error'=>'no login']);
+			return $callback.'(100)';exit;
 		}		
-		$Website = new Website();
-		$website_info = $Website->alias('w')->field("at.file_path icon_path,att.file_path logo_path")
-			->join('attachment at','at.id=w.icon_id','LEFT')
-			->join('attachment att','att.id=w.logo_id','LEFT')
-			->find();
-		echo '<script type="text/javascript">window.location.href="http://www.baidu.com";</script>';exit;
 		return $callback.'(100)';exit;
-		return json_encode(['status'=>'ok', 'data'=>[
-				'empuser_id'=>session('empuser_id'), 
-				'empuser_uname'=>session('empuser_uname'),
-				'icon_path'=>$website_info['icon_path'],
-				'logo_path'=>$website_info['logo_path']
-			]
-		]);
 	}
 }
