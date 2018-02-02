3ea98e0a1e2bf1939edec599589157850abdf08f
1226151667 <1226151667@qq.com>
2018-02-02 10:55:08 +0800

    we

--- admin/index/controller/Nolapi.php
+++ admin/index/controller/Nolapi.php
@@ -7,8 +7,8 @@ class Nolapi extends Controller{
 	public function login_status(){
 		$callback = request()->get('callback');
 		if(!session('?empuser_uname') || !session('?empuser_id')){
-			return $callback.'("no","login error")';exit;
+			return $callback.'("no","login error");';
 		}		
-		return $callback.'("ok",'.md5(session('empuser_id')).')';
+		return $callback.'("ok",'.md5(session('empuser_id')).');';
 	}
 }
